34d4bfaa94c00fe14c52c4c90e2da6ad8541bf8f
Nikhil Lama <nikhil.lama@ekbana.info>
2023-02-11 21:49:39 +0545

    feat: remove unused codes and create signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import { Typography, TextField, Box, Button } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const handleSubmit = () => {
+		console.log('asd');
+	};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className='App'>
+			<Typography variant='h4' fontWeight='bold'>
+				WELCOME
+			</Typography>
+			<Box
+				display='flex'
+				flexDirection='column'
+				justifyContent='center'
+				component='form'
+				width={600}
+				sx={{ backgroundColor: '#f5f5f5', m: '1rem auto 0 auto' }}
+				padding={4}
+				borderRadius={10}
+				onSubmit={handleSubmit}>
+				<TextField label='First Name' />
+				<TextField sx={{ mt: 2 }} label='Last Name' />
+				<TextField sx={{ mt: 2 }} label='E-mail' />
+				<TextField sx={{ mt: 2 }} label='Address' />
+				<TextField sx={{ mt: 2 }} label='Password' />
+				<TextField sx={{ mt: 2 }} label='Confirm Password' />
+				<Button sx={{ marginTop: 2 }} variant='contained'>
+					Submit
+				</Button>
+			</Box>
+		</div>
+	);
 }
 
-export default App
+export default App;
