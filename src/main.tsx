f4fe74a6da38bdf3d87ba234408e7ebdab723c3d
Nikhil Lama <nikhil.lama@ekbana.info>
2023-02-14 23:16:29 +0545

    feat: integrate react-query and update form with an immutability state

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { QueryClientProvider } from 'react-query';
+import { QueryClient } from 'react-query';
 
 const theme = createTheme({});
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
