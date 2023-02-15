5a59931b123cdedbca88db49a70d19e4e73932b9
Nikhil Lama <nikhil.lama@ekbana.info>
2023-02-15 23:21:20 +0545

    feat: setup axios instance

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const API = axios.create({
-	baseURL: 'http:localhost:8000/api/v1',
+	baseURL: 'http://localhost:8000/api',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	withCredentials: true,
 });
